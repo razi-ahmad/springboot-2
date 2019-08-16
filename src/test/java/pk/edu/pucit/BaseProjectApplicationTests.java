36dd5c364b62ef905f997a2e3853f3ddc8bc8d37
VenD-Razi <razi.ahmad@venturedive.com>
2019-08-17 02:55:14 +0500

    First commit

---
+++ src/test/java/pk/edu/pucit/BaseProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package pk.edu.pucit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaseProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
